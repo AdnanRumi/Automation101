ce6bcc09acbfc4617e0bc6c1914419362175752a
AdnanRumi <mugterib@gmail.com>
2022-05-13 21:05:08 +0200

    Task for Saadet

---
+++ src/main/java/Task1.java
@@ -0,0 +1,9 @@
+public class Task1 {
+// Fetch the repo from the remote
+// Create a new called May13
+// Checkout to that branch
+// Under java package create a new java class named "TC101New and make a commit with a message "Task1-1"
+// Create a main method in it and build a print statement that outputs "Peace be with you"
+// Commit this print statement changes with the message "Task1-Final"
+// Push everything to the remote to a new remote branch called May13
+}
