942dccb933d8363992dd936047cfcc3c729c3838
Saadet <99473429+sakoyaespana@users.noreply.github.com>
2022-05-13 20:25:59 +0200

    Flower3

--- src/main/java/Test101_03.java
+++ src/main/java/Test101_03.java
@@ -2,13 +2,12 @@ public class Test101_03 {
 
     public static void main(String[] args) {
     
-    WebDriverManager.chromedriver().setup();
-    
-    WebDriver driver = new ChromeDriver();
-    
-    driver.navigate().to("https://www.cybertekschool.com");
-    
-    
-        
+//    WebDriverManager.chromedriver().setup();
+//
+//    WebDriver driver = new ChromeDriver();
+//
+//    driver.navigate().to("https://www.cybertekschool.com");
+//   wooow
+
     }
 }
