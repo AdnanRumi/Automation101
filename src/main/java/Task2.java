c6cabfe6e9f8156beb0b55b85b277908413fdce1
AdnanRumi <mugterib@gmail.com>
2022-05-13 22:18:17 +0200

    Task2 for Saadet

---
+++ src/main/java/Task2.java
@@ -0,0 +1,14 @@
+public class Task2 {
+
+// Fetch the repo from the remote
+// Create a new branch called Mayhem
+// Checkout to that branch
+// Under java package use this java class and declare "joe", "moe", and "boe" variables and make a commit with a message "Mayhem-Triplets"
+// Create a main method in it and build a print statement that outputs "Hullo" and make a commit with a message "Mayhem-1"
+// Create another method in this class called "roundRobin" and build a print statement that outputs "Hunky dory" and make a commit with a message "Mayhem-2"
+// Push everything to the remote to a new remote branch called MayhemOrigin
+// Create a pull request
+
+//    Write the code here
+
+}
